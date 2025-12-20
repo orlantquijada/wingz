f9dab7e53e5ee0ef8e4c85804e83b374734d2f5f
Orlan Quijada <orlantquijada@gmail.com>
2025-12-20 23:52:40 +0800

    add DRF package

--- api/settings.py
+++ api/settings.py
@@ -40,6 +40,8 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    # Third-party apps
+    "rest_framework",
     # Local apps
     "users",
     "rides",
