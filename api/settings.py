a6489c0d953b405af7419092da8792fa207eef3f
Orlan Quijada <orlantquijada@gmail.com>
2025-12-21 03:15:29 +0800

    add default pagination

--- api/settings.py
+++ api/settings.py
@@ -60,6 +60,8 @@ REST_FRAMEWORK = {
     "DEFAULT_PERMISSION_CLASSES": [
         "api.permissions.IsAdminUser",
     ],
+    "DEFAULT_PAGINATION_CLASS": "rest_framework.pagination.PageNumberPagination",
+    "PAGE_SIZE": 10,
 }
 
 SIMPLE_JWT = {
